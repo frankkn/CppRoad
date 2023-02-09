9f591d654ba254b111c182ab81bc5d495e50946c
Frank <frank840629@gmail.com>
2023-02-09 08:25:19 +0800

    update

--- Cpp_exercise/OOP/Encapsulation_Inheritance/shape.h
+++ Cpp_exercise/OOP/Encapsulation_Inheritance/shape.h
@@ -15,6 +15,7 @@ class Circle:public Shape{
     int radius; 
   public:
     Circle():radius(0) {}
+    Circle(int r):radius(r) {}
     void setRadius(int r);
 };
 
@@ -23,6 +24,7 @@ class Rectangle:public Shape{
     int width;
   public:
     Rectangle():height(0), width(0) {}
+    Rectangle(int h, int w):height(h), width(w) {}
     void setHeight(int h);
     void setWidth(int w);
 };
